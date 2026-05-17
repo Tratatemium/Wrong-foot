6f05b1553367861c8fe26885d8681e163a6d060f
Vladislav Zhuravskii <visitanoinvitado@gmail.com>
2026-05-17 17:24:35 +0200

    Refactor: improve story play function by using within for canvas element

--- src/components/ui/Title/Title.stories.tsx
+++ src/components/ui/Title/Title.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
+import { userEvent, within } from "storybook/test";
 import { Title } from "./Title";
 
 const meta = {
@@ -23,8 +24,8 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {};
 
 export const Editing: Story = {
-  play: async ({ canvas }) => {
-    const { userEvent } = await import("storybook/test");
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
     const editButton = canvas.getByRole("button", { name: /edit title/i });
     await userEvent.click(editButton);
   },
